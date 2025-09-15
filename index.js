1482e8c29125bf35346dfb8da491dd4ac072b02c
Nikhilroy22 <nikhilroyfff@gmail.com>
2025-09-15 21:15:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// EJS সেট করা
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Static ফাইলের জন্য
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index', { title: 'রিয়েল-টাইম চ্যাট' });
+});
+
+// Socket.IO
+io.on('connection', (socket) => {
+  console.log('একজন ইউজার কানেক্টেড');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('ইউজার ডিসকানেক্ট');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server চলছে: http://localhost:3000');
+});
