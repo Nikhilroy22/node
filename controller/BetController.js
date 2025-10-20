3601db898115f7d1cf2900ea2ddeb231df071a2d
Nikhilroy22 <nikhilroyfff@gmail.com>
2025-10-20 18:40:18 +0600

    first commit

---
+++ controller/BetController.js
@@ -0,0 +1,32 @@
+
+
+//Bet Home Page
+
+export.bethome = (req, res) => {
+  
+  res.render('bet');
+}
+
+
+//Place Bet Post
+export.placeBet = (req, res) => {
+  
+  res.json('bet');
+}
+
+
+// Bet json API
+export.betapi = async(req, res) =>{
+  const url = "https://1xbet86.com/LiveFeed/Get1x2_VZip?sports=16&count=50&lng=en&gr=54&antisports=4&mode=4&country=19&getEmpty=true";
+
+const fdata = await fetch(url, {
+    method: "GET",
+    headers: {
+      "User-Agent": "Mozilla/5.0",     // বা অন্য header প্রয়োজনে
+      "Accept": "application/json"
+    }
+  });
+const jjj = await fdata.json();
+res.json(jjj);
+  
+}
--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -6,33 +6,13 @@ const router = express.Router();
 const authController = require('../controller/LoginController');
 const SignUpController = require('../controller/SignupController')
 const Home = require('../controller/HomeController');
-
 const authMiddleware = require('../middleware/authMiddleware');
+const BetController = require('../middleware/BetController');
 
-
-router.get("/res", async(req, res) =>{
-  const url = "https://1xbet86.com/LiveFeed/Get1x2_VZip?sports=16&count=50&lng=en&gr=54&antisports=4&mode=4&country=19&getEmpty=true";
-
-const fdata = await fetch(url, {
-    method: "GET",
-    headers: {
-      "User-Agent": "Mozilla/5.0",     // বা অন্য header প্রয়োজনে
-      "Accept": "application/json"
-    }
-  });
-const jjj = await fdata.json();
-res.json(jjj);
-  
-});
-router.get('/bet', (req, res) => {
-  
-  res.render('bet');
-});
-
-router.post('/bet', (req, res) => {
-  
-  res.json('bet');
-});
+// Bet ROUTE
+router.get("/res", BetController.betapi);
+router.get('/bet', BetController.bethome);
+router.post('/bet', BetController.placeBet);
 
 
 //Crash page
