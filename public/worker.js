cd6d845af96d68233da1821131d30af70c2890ae
nikhilroy22 <nikhilroyfff@gmail.com>
2025-11-25 23:54:48 +0600

    first commit

--- public/worker.js
+++ public/worker.js
@@ -1,22 +1,42 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/logo.png'
+];
+
+// Install event: ফাইলগুলো ক্যাশে রাখবে
 self.addEventListener('install', event => {
-  console.log('Service Worker installing...');
+  console.log('[ServiceWorker] Install');
   event.waitUntil(
-    caches.open('my-cache-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/bet,
-        '/css/style.css',
-        '/js/script.js'
-      ]);
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event: পুরোনো ক্যাশ ক্লিয়ার করা
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated');
-});
-
+// Fetch event: নেটওয়ার্কে গেলে ক্যাশ থেকে serve করবে, না গেলে নেটওয়ার্কে যাবে
 self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] Fetch', event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then(response => {
