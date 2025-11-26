7c77daa619ee6348e8578ff9e0280c175bbe51cf
nikhilroy22 <nikhilroyfff@gmail.com>
2025-11-26 18:18:15 +0600

    first commit

--- public/adminjs/script.js
+++ public/adminjs/script.js
@@ -1,3 +1,8 @@
 function toggleMenu(){
   document.getElementById('sidebar').classList.toggle('active');
 }
+
+function closenavbar(){
+  document.getElementById('sidebar').classList.toggle('active');
+  
+}
