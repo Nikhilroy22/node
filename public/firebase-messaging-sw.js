6ca36d73ed15b1a46ed83c93838310896fb1f0d3
nikhilroy22 <nikhilroyfff@gmail.com>
2025-11-27 13:02:05 +0600

    first commit

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+// public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyDa1jib5JJfboOMkhw64Nwl6ia_LfcOoRk",
   authDomain: "node-dc510.firebaseapp.com",
   projectId: "node-dc510",
@@ -10,13 +10,13 @@ const firebaseConfig = {
   messagingSenderId: "917525434182",
   appId: "1:917525434182:web:265054ff20ea2a08409f47",
   measurementId: "G-B6RK58PGN2"
-};
+});
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: "/icon.png"
+    icon: '/icon.png'
   });
 });
