9cf6654e5a17e0b34eb1c57c35d12d3c26214776
nikhilroy22 <nikhilroyfff@gmail.com>
2025-11-27 12:44:13 +0600

    first commit

--- helper/firebase.js
+++ helper/firebase.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 const path = require("path");
 
 // Absolute path to your service account JSON
-const serviceAccount = require(path.resolve(__dirname, "node-dc510-firebase-adminsdk-fbsvc-7b986629b5.json"));
+const serviceAccount = require(path.resolve(__dirname, "nj.json"));
 
 // Initialize Admin SDK
 admin.initializeApp({
