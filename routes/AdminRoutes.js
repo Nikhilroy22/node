bfa9c3ec63d4d5313d41cef7272effc6a0bab043
nikhilroy22 <nikhilroyfff@gmail.com>
2026-01-19 22:37:49 +0600

    first commit

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -1,43 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const admin = require('../middleware/IsAdmin');
+
 const file = require('../controller/FileManagerController');
 const post = require('../controller/BlogController');
-
 const jj = require('../db/nodesqlite');
 
+// 🔒 সব route-এর আগে middleware
+router.use(admin);
+
+
 router.get("/data", (req, res) => {
-  // INSERT
-/*jj.db.prepare(
-  "INSERT INTO users (name, age) VALUES (?, ?)"
-).run("Nikhil", 25);
-*/
-// SELECT
-const id = 1;
-
-const row = jj.db.prepare(
-  "SELECT * FROM users"
-).all();
-
-//console.log(row);
-
+  const row = jj.db.prepare("SELECT * FROM users").all();
   res.json(row);
 });
 
-router.get('/', (req, res)=> {
-  
+router.get('/', (req, res) => {
   res.render('admin/home');
-  
 });
-//SETTINGS
+
 router.get("/setting", (req, res) => {
   res.render("admin/Setting");
-  
 });
 
-// post
-router.get('/addpost', post.blogview )
-router.post('/savepost', post.savepost )
-
+// Blog
+router.get('/addpost', post.blogview);
+router.post('/savepost', post.savepost);
 
 // File Manager
 router.get('/filemanager', file.fileview);
